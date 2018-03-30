128f15ced80865690b7ac505f3d46bc6be24c393
Kazuho Oku <kazuhooku@gmail.com>
2018-03-30 14:31:19 +0900

    rebuild SSL_CTX when receiving SIGHUP

--- test.c
+++ test.c
@@ -20,6 +20,7 @@
  * IN THE SOFTWARE.
  */
 #include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
@@ -28,6 +29,11 @@
 #include <openssl/crypto.h>
 #include <openssl/ssl.h>
 #include "neverbleed.h"
+
+static neverbleed_t nb;
+static char errbuf[NEVERBLEED_ERRBUF_SIZE];
+static const char *certfn, *keyfn;
+static int use_privsep, reload = 1;
 
 static void setup_ecc_key(SSL_CTX *ssl_ctx)
 {
@@ -41,10 +47,11 @@ static void setup_ecc_key(SSL_CTX *ssl_ctx)
     EC_KEY_free(key);
 }
 
-int dumb_https_server(unsigned short port, SSL_CTX *ctx)
+int dumb_https_server(unsigned short port)
 {
-    int listen_fd, reuse_flag;
+    int listen_fd, conn_fd, reuse_flag;
     struct sockaddr_in sin = {};
+    SSL_CTX *ctx;
 
     if ((listen_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         fprintf(stderr, "failed to create socket:%s\n", strerror(errno));
@@ -54,7 +61,7 @@ int dumb_https_server(unsigned short port, SSL_CTX *ctx)
     setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &reuse_flag, sizeof(reuse_flag));
     sin.sin_family = AF_INET;
     sin.sin_addr.s_addr = htonl(0x7f000001);
-    sin.sin_port = htons(8888);
+    sin.sin_port = htons(port);
     if (bind(listen_fd, (void *)&sin, sizeof(sin)) != 0) {
         fprintf(stderr, "bind failed:%s\n", strerror(errno));
         return 111;
@@ -65,39 +72,60 @@ int dumb_https_server(unsigned short port, SSL_CTX *ctx)
     }
 
     while (1) {
-        int conn_fd;
-        SSL *ssl;
-        char buf[4096];
+        /* (re)load SSL context */
+        if (reload) {
+            reload = 0;
+            if (ctx != NULL)
+                SSL_CTX_free(ctx);
+            fprintf(stderr, "loading certificate and private key\n");
+            ctx = SSL_CTX_new(SSLv23_server_method());
+            SSL_CTX_set_options(ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION);
+            setup_ecc_key(ctx);
+            if (SSL_CTX_use_certificate_chain_file(ctx, certfn) != 1) {
+                fprintf(stderr, "failed to load certificate chain file:%s\n", certfn);
+                return 111;
+            }
+            if (use_privsep) {
+                if (neverbleed_load_private_key_file(&nb, ctx, keyfn, errbuf) != 1) {
+                    fprintf(stderr, "failed to load private key from file:%s:%s\n", keyfn, errbuf);
+                    exit(111);
+                }
+            } else {
+                if (SSL_CTX_use_PrivateKey_file(ctx, keyfn, SSL_FILETYPE_PEM) != 1) {
+                    fprintf(stderr, "failed to load private key from file:%s\n", keyfn);
+                    exit(111);
+                }
+            }
+        }
         /* accept connection */
-        while ((conn_fd = accept(listen_fd, NULL, NULL)) == -1 && errno == EINTR)
-            ;
-        if (conn_fd == -1) {
-            fprintf(stderr, "accept(2) failed:%s\n", strerror(errno));
-            return 111;
+        if ((conn_fd = accept(listen_fd, NULL, NULL)) != -1) {
+            char buf[4096];
+            SSL *ssl = SSL_new(ctx);
+            SSL_set_fd(ssl, conn_fd);
+            if (SSL_accept(ssl) == 1) {
+                SSL_read(ssl, buf, sizeof(buf));
+                const char *resp =
+                    "HTTP/1.0 200 OK\r\nContent-Length: 6\r\nConnection: close\r\nContent-Type: text/plain\r\n\r\nhello\n";
+                SSL_write(ssl, resp, strlen(resp));
+                SSL_shutdown(ssl);
+            } else {
+                fprintf(stderr, "SSL_accept failed\n");
+            }
+            SSL_free(ssl);
+            close(conn_fd);
         }
-        ssl = SSL_new(ctx);
-        SSL_set_fd(ssl, conn_fd);
-        if (SSL_accept(ssl) == 1) {
-            SSL_read(ssl, buf, sizeof(buf));
-            const char *resp =
-                "HTTP/1.0 200 OK\r\nContent-Length: 6\r\nConnection: close\r\nContent-Type: text/plain\r\n\r\nhello\n";
-            SSL_write(ssl, resp, strlen(resp));
-            SSL_shutdown(ssl);
-        } else {
-            fprintf(stderr, "SSL_accept failed\n");
-        }
-        SSL_free(ssl);
-        close(conn_fd);
     }
+}
+
+static void on_sighup(int signo)
+{
+    write(2, "sighup!\n", 8);
+    reload = 1;
 }
 
 int main(int argc, char **argv)
 {
     unsigned short port;
-    SSL_CTX *ctx;
-    neverbleed_t nb;
-    char errbuf[NEVERBLEED_ERRBUF_SIZE];
-    int use_privsep;
 
     /* initialization */
     SSL_load_error_strings();
@@ -107,10 +135,6 @@ int main(int argc, char **argv)
         fprintf(stderr, "openssl_privsep_init: %s\n", errbuf);
         return 111;
     }
-    ctx = SSL_CTX_new(SSLv23_server_method());
-    SSL_CTX_set_options(ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION);
-    setup_ecc_key(ctx);
-
     /* parse args */
     if (argc != 5) {
         fprintf(stderr, "Usage: %s <internal|privsep> <port> <certificate-chain-file> <private-key-file>\n", argv[0]);
@@ -128,22 +152,17 @@ int main(int argc, char **argv)
         fprintf(stderr, "failed to parse port:%s\n", argv[2]);
         return 111;
     }
-    if (SSL_CTX_use_certificate_chain_file(ctx, argv[3]) != 1) {
-        fprintf(stderr, "failed to load certificate chain file:%s\n", argv[3]);
-        return 111;
-    }
-    if (use_privsep) {
-        if (neverbleed_load_private_key_file(&nb, ctx, argv[4], errbuf) != 1) {
-            fprintf(stderr, "failed to load private key from file:%s:%s\n", argv[4], errbuf);
-            return 111;
-        }
-    } else {
-        if (SSL_CTX_use_PrivateKey_file(ctx, argv[4], SSL_FILETYPE_PEM) != 1) {
-            fprintf(stderr, "failed to load private key from file:%s\n", argv[4]);
-            return 111;
-        }
+    certfn = argv[3];
+    keyfn = argv[4];
+
+    {
+        struct sigaction sa;
+        memset(&sa, 0, sizeof(sa));
+        sigemptyset(&sa.sa_mask);
+        sa.sa_handler = on_sighup;
+        sigaction(SIGHUP, &sa, NULL);
     }
 
     /* start the httpd */
-    return dumb_https_server(port, ctx);
+    return dumb_https_server(port);
 }
